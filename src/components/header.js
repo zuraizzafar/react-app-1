fd21eac395ea384ec109b78ae99dd8b14908194b
zuraizzafar <zuraiz49@gmail.com>
2021-04-23 15:37:05 +0500

    Learned React components.

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import Title from './title';
+import Button from './button';
+
+function Header() {
+    return (
+        <div>
+            <Title/>
+            <p>This is Headear paragraph</p>
+            <Button/>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/components/title.js
@@ -0,0 +1,7 @@
+function Title() {
+    return (
+        <h1>This is title.</h1>
+    )
+}
+
+export default Title;
